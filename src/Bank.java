21f064fbf442fd23e351cc5c967025177a201144
mercy <mercymugambi2016@gmail.com>
2021-09-22 16:12:08 +0300

    first commit

---
+++ src/Bank.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class Bank {
+	
+	/**
+	 * What data should a bank have?
+	 */
+	
+	/**
+	 * The name of the bank
+	 */
+	private String name;
+	
+	/**
+	 * List of User or bank customers
+	 */
+	private ArrayList<User> user;
+	
+	/**
+	 *List of accounts users have 
+	 */
+	private ArrayList<Account> account;
+	
+	
+	
+	
+	
+
+}
